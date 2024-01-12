9948e831414d043cdbfecdf6313088fe1c0e4025
pisarraguido <pisarraguido@gmail.com>
2024-01-12 19:54:58 -0300

    estructuras de control

--- whitetower.py
+++ whitetower.py
@@ -1,2 +1,25 @@
-print ("chupapija")
-print ("so vo")
+print("chupapija")
+print("so vo")
+
+
+my_condition = 5 > 8
+
+if my_condition:
+    print("La condición es verdadera")
+else:
+    print("La condición es falsa")
+    print("false")
+
+number_one = 1
+
+while number_one < 5:
+    print(number_one)
+    #    if number_one == 3:
+    #       break
+    number_one += 1
+
+
+my_list = [23, 25, 30, 37]
+
+for element in my_list:
+    print(element)
